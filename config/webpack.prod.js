ea47f8d14d4e7381111059e23c06848aa5a2795d
rangaraj.s <rangaraj.sam@gmail.com>
2026-03-19 18:07:16 +0530

    profile webpack and package files updated

---
+++ config/webpack.prod.js
@@ -0,0 +1,31 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+const {ModuleFederationPlugin} = require('webpack').container;
+const packageJson = require('../package.json');
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    publicPath: '/profile/latest/',
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'profile',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './ProfileApp': './src/bootstrap',
+            },
+            remotes: {
+                container: `container@${domain}/container/latest/remoteEntry.js`
+            },
+            shared: {
+                ...packageJson.dependencies,
+                mitt: { singleton: true, strictVersion: false, requiredVersion: false }
+            }
+        }),
+    ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
